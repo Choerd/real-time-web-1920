7d4592069928fbe418093d5ac667863065a39f43
Sjors <sjorspieter@live.nl>
2020-04-08 13:07:56 +0200

    weird bug

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const
     express = require('express'),
     app = express(),
+    http = require('http').createServer(app),
+    io = require('socket.io')(http),
     expressLayouts = require('express-ejs-layouts')
 
 app
@@ -12,8 +14,49 @@ app
     .use(expressLayouts)
     .use(express.static('production'))
 
-const indexRouter = require('./routes')
+// socket.io
+let counter = 0
 
-app.use('/', indexRouter)
+io.on('connection', socket => {
+    const id = counter++
 
-app.listen(process.env.PORT || 4000, () => console.log(`Listening on Port ${process.env.PORT || 4000}`))
+    // A user joins the room
+    socket.emit('join', {
+        user: 'server',
+        message: `You (${id}) joined the chat!`
+    })
+
+    socket.broadcast.emit('join', {
+        user: 'server',
+        message: `Anonymous(${id}) joined the chat!`
+    })
+
+    // A user leaves the chat
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leave', {
+            user: 'server',
+            message: `Anonymous(${id}) left the chat!`
+        })
+    })
+
+    // Chatting
+    socket.on('chat', (user) => {
+        if (user.message.includes('/')) {
+            user.name = `${user.name} (${id})`
+            io.sockets.emit('command', user)
+        } else if (user.message.includes(':')) {
+            user.name = `${user.name} (${id})`
+            io.sockets.emit('emote', user)
+        } else {
+            user.name = `${user.name} (${id})`
+            io.sockets.emit('chat', user)
+
+        }
+    })
+})
+
+const chatRouter = require('./routes/chat')
+
+app.use('/', chatRouter)
+
+http.listen(process.env.PORT || 4000, () => console.log(`Listening on Port ${process.env.PORT || 4000}`))
