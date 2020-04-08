a0fb24168f6b82fd9c7b4572c4c1219a51f217ee
Sjors <sjorspieter@live.nl>
2020-04-08 12:29:22 +0200

    small fix boilerplate

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ require('dotenv').config()
 const
     express = require('express'),
     app = express(),
-    http = require('http').createServer(app),
-    io = require('socket.io')(http),
     expressLayouts = require('express-ejs-layouts')
 
 app
@@ -18,4 +16,4 @@ const indexRouter = require('./routes')
 
 app.use('/', indexRouter)
 
-http.listen(process.env.PORT || 4000, () => console.log(`Listening on Port ${process.env.PORT || 4000}`))
+app.listen(process.env.PORT || 4000, () => console.log(`Listening on Port ${process.env.PORT || 4000}`))
